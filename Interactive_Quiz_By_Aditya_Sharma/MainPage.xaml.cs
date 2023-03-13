49ed3305a45152ae8b7648284e6d9916468ebd63
Addy768 <adityasharma02092002@gmail.com>
2023-03-13 18:06:13 -0400

    Almost Finished

--- Interactive_Quiz_By_Aditya_Sharma/MainPage.xaml.cs
+++ Interactive_Quiz_By_Aditya_Sharma/MainPage.xaml.cs
@@ -4,49 +4,176 @@ public partial class MainPage : ContentPage
 {
 	public string _correctanswer;
 	Quiz _quiz = new Quiz("My Quiz");
+    public string userAnswer;
+
 
 	public MainPage()
 	{
 		InitializeComponent();
        _quiz.Title = title.Text;
-        MultipleChoiceQuestion ques = _quiz.GetNextQuestion();
-        Console.WriteLine("Result fo Next Ques method ::::: " +ques);
-        question.Text = ques.QuestionText;
-        Option1.Text = ques.options[0];
-        Option2.Text = ques.options[1];
-        Option3.Text = ques.options[2];
-        Option4.Text = ques.options[3];
+        _quiz.Title = "GENERAL KNOWLEDGE";
+        title.Text = _quiz.Title;
+        GetQuestion();
+        GetOptions();
+
 
     }
 
-    //private void OnCounterClicked(object sender, EventArgs e)
-    //{
+    public void GetQuestion()
+    {
+        Question ques = _quiz.GetNextQuestion();
+        question.Text = ques.QuestionText;
+    }
 
-    //}
+
+
+    public void GetOptions()
+    {
+        String userAnswer;
+        int _currentQuestionIndex = _quiz.GetCurrentques();
+        if (_currentQuestionIndex <= 5)
+        {
+            Option1.Text = _quiz.OnOption1();
+            Option2.Text = _quiz.OnOption2();
+            Option3.Text = _quiz.OnOption3();
+            Option4.Text = _quiz.OnOption4();
+            userAnswer = _quiz.ForAnswer();
+        }
+        if (_currentQuestionIndex > 5)
+        {
+            Option1.IsVisible = false;
+            Option2.Text = "True";
+            Option3.Text = "False";
+            Option4.IsVisible = false;
+            userAnswer = _quiz.ForAnswer();
+        }
+
+    }
 
     private void OnOption1Clicked(object sender, EventArgs e)
     {
-        //_correctanswer = question.Text;
 
-        //SemanticScreenReader.Announce(Option1.Text);
+        userAnswer = _quiz.OnOption1();
+
+
+        if (_quiz.CheckUserAnswer( userAnswer))
+        {
+            Option1.BackgroundColor = Colors.Green;
+        }
+        else
+        {
+            Option1.BackgroundColor = Colors.Red;
+        }
+        Option4.IsVisible = false;
+        Option2.IsVisible = false;
+        Option3.IsVisible = false;
+
 
     }
 
 
     private void OnOption2Clicked(object sender, EventArgs e)
     {
-        
+        if (_quiz.GetCurrentques() <= 4)
+        {
+            userAnswer = _quiz.OnOption2();
+        }
+        else
+        {
+            userAnswer = "True";
+        }
+
+        if (_quiz.CheckUserAnswer(userAnswer))
+        {
+            Option2.BackgroundColor = Colors.Green;
+        }
+        else
+        {
+            Option2.BackgroundColor = Colors.Red;
+        }
+        Option1.IsVisible = false;
+        Option3.IsVisible = false;
+        Option4.IsVisible = false;
 
     }
     private void OnOption3Clicked(object sender, EventArgs e)
     {
-   
+        if (_quiz.GetCurrentques() <= 4)
+        {
+            userAnswer = _quiz.OnOption3();
+        }
+        else
+        {
+            userAnswer = "False";
+        }
+
+
+        if (_quiz.CheckUserAnswer(userAnswer))
+        {
+            Option3.BackgroundColor = Colors.Green;
+
+        }
+        else
+        {
+            Option3.BackgroundColor = Colors.Red;
+        }
+        Option1.IsVisible = false;
+        Option2.IsVisible = false;
+        Option4.IsVisible = false;
+
 
     }
     private void OnOption4Clicked(object sender, EventArgs e)
     {
-      
+        userAnswer = _quiz.OnOption4();
 
+
+        if (_quiz.CheckUserAnswer(userAnswer))
+        {
+            Option4.BackgroundColor = Colors.Green;
+        }
+        else
+        {
+            Option4.BackgroundColor = Colors.Red;
+        }
+        Option1.IsVisible = false;
+        Option2.IsVisible = false;
+        Option3.IsVisible = false;
+
+
+
+
+
+    }
+    private void OnNextClicked(object sender, EventArgs e)
+    {
+
+        if (_quiz.GetCurrentques() == 10)
+        {
+
+            DisplayAlert("Score", $"You scored {_quiz.Score} out of {_quiz.GetCurrentques()} questions. You got {_quiz.Point} points out of {_quiz.ForTotalPoints()}", "OK");
+
+        }
+        else
+        {
+            GetQuestion();
+            GetOptions();
+            Resetebtn();
+
+        }
+
+
+
+    }
+    public void Resetebtn()
+    {
+        Button[] buttons = { Option1, Option2, Option3, Option4 };
+
+        foreach (Button button in buttons)
+        {
+            button.BackgroundColor = Colors.White;
+            button.IsEnabled = true;
+        }
     }
 }
 
--- Interactive_Quiz_By_Aditya_Sharma/Quiz.cs
+++ Interactive_Quiz_By_Aditya_Sharma/Quiz.cs
@@ -9,13 +9,15 @@ namespace Interactive_Quiz_By_Aditya_Sharma
     public class Quiz
     {
         //Quiz _quiz = new Quiz("My Quiz");
-        private List<MultipleChoiceQuestion> _questionList1 = new List<MultipleChoiceQuestion>();
-        private List<TrueFalseQuestion> _questionList2 = new List<TrueFalseQuestion>();
-        private int _currentQuestion = -1;
+        //private List<MultipleChoiceQuestion> _questionList1 = new List<MultipleChoiceQuestion>();
+        //private List<TrueFalseQuestion> _questionList2 = new List<TrueFalseQuestion>();
+        private List<Question> _questionList = new List<Question>();
+        private int _currentQuestionIndex = -1;
         private string _title;
         private int _score;
-        private MultipleChoiceQuestion quesWithOptions = new MultipleChoiceQuestion();
-        private TrueFalseQuestion tfQuesWithOptions = new TrueFalseQuestion();
+        public int _point;
+        //private MultipleChoiceQuestion quesWithOptions = new MultipleChoiceQuestion();
+        //private TrueFalseQuestion tfQuesWithOptions = new TrueFalseQuestion();
 
         public string Title
         {
@@ -41,6 +43,18 @@ namespace Interactive_Quiz_By_Aditya_Sharma
             }
         }
 
+        public int Point { 
+            get
+            {
+                return _point;  
+
+            } 
+            set
+            {
+                _point = value; 
+            } 
+        }
+
         public Quiz(string title)
         {
             _title = title;
@@ -48,7 +62,7 @@ namespace Interactive_Quiz_By_Aditya_Sharma
             LoadQuestions();
         }
 
-        /*
+        
          private void LoadQuestions()
          {
              // Add 5 multiple choice questions
@@ -58,33 +72,166 @@ namespace Interactive_Quiz_By_Aditya_Sharma
              _questionList.Add(new MultipleChoiceQuestion("What team won 2022 football worldcup ?", "Argentina", 1, new string[] { "Germany", "France", "Morocco" }));
              _questionList.Add(new MultipleChoiceQuestion("Which country is known as land of maple syrup", "Canada", 1, new string[] { "Chine", "USA", "Russia" }));
 
-             // Add 5 true/false questions
-             _questionList.Add(new TrueFalseQuestion("The Great Wall of China is visible from space.", 1, false));
-             _questionList.Add(new TrueFalseQuestion("The human body has three lungs.", 1, false));
-             _questionList.Add(new TrueFalseQuestion("The Earth is square.", 1, false));
-             _questionList.Add(new TrueFalseQuestion("The Statue of Liberty is in Germany.", 1, false));
-             _questionList.Add(new TrueFalseQuestion("The sun rises in the east and sets in the west.", 1, true));
-
-         }
-        */
-
-        private void LoadQuestions()
-        {
-            // Add 5 multiple choice questions
-            _questionList1.Add(new MultipleChoiceQuestion("Which country held 2022 football worldcup?", "Qatar", 1, new string[] { "USA", "China", "Canada", "Qatar" }));
-            _questionList1.Add(new MultipleChoiceQuestion("What is the nation capital of India ?", "Delhi", 1, new string[] { "Maharashtra", "Rajasthan", "Kerala" }));
-            _questionList1.Add(new MultipleChoiceQuestion("Which is the largest mammal?", "Blue Whale", 1, new string[] { "Elephant", "Giraffe", "Hippopotamus" }));
-            _questionList1.Add(new MultipleChoiceQuestion("What team won 2022 football worldcup ?", "Argentina", 1, new string[] { "Germany", "France", "Morocco" }));
-            _questionList1.Add(new MultipleChoiceQuestion("Which country is known as land of maple syrup", "Canada", 1, new string[] { "Chine", "USA", "Russia" }));
-
+          
             // Add 5 true/false questions
-            _questionList2.Add(new TrueFalseQuestion("The Great Wall of China is visible from space.", "false", 1, new string[] {"true", "false"}));
-            _questionList2.Add(new TrueFalseQuestion("The human body has three lungs.", "false", 1, new string[] { "true", "false" }));
-            _questionList2.Add(new TrueFalseQuestion("The Earth is square.", "false", 1, new string[] { "true", "false" }));
-            _questionList2.Add(new TrueFalseQuestion("The Statue of Liberty is in Germany.", "false", 1, new string[] { "true", "false" }));
-            _questionList2.Add(new TrueFalseQuestion("The sun rises in the east and sets in the west.", "true", 1, new string[] { "true", "false" }));
+            _questionList.Add(new TrueFalseQuestion("The Great Wall of China is visible from space.", "false", 1, new string[] { "true", "false" }));
+            _questionList.Add(new TrueFalseQuestion("The human body has three lungs.", "false", 1, new string[] { "true", "false" }));
+            _questionList.Add(new TrueFalseQuestion("The Earth is square.", "false", 1, new string[] { "true", "false" }));
+            _questionList.Add(new TrueFalseQuestion("The Statue of Liberty is in Germany.", "false", 1, new string[] { "true", "false" }));
+            _questionList.Add(new TrueFalseQuestion("The sun rises in the east and sets in the west.", "true", 1, new string[] { "true", "false" }));
+        }
+        
+        public String OnOption1()
+        {
+            MultipleChoiceQuestion question = (MultipleChoiceQuestion)_questionList[_currentQuestionIndex];
+            String option = question.options[0];
+            return option;
+        }
+        public String OnOption2()
+        {
+            MultipleChoiceQuestion question = (MultipleChoiceQuestion)_questionList[_currentQuestionIndex];
+            String option = question.options[1];
+            return option;
+        }
+        public String OnOption3()
+        {
+            MultipleChoiceQuestion question = (MultipleChoiceQuestion)_questionList[_currentQuestionIndex];
+            String option = question.options[2];
+            return option;
+        }
+        public String OnOption4()
+        {
+            MultipleChoiceQuestion question = (MultipleChoiceQuestion)_questionList[_currentQuestionIndex];
+            String option = question.options[3];
+            return option;
+        }
+        
+        public int GetCurrentques()
+        {
+            return _currentQuestionIndex;
+        }
+//loads the question with the option of the question with answer being blank
+        private Question GetQuestionWithoutAnswer()
+        {
+            if (_currentQuestionIndex < _questionList.Count())
+            {
+                Question question = _questionList[_currentQuestionIndex];
+                Question _questionWithoutAnswer = new Question
+                {
+                    QuestionText = question.QuestionText,
+                    Points = question.Points,
+
+                    CorrectAnswer = "",
+                };
+
+                return _questionWithoutAnswer;
+
+            }
+            else
+            {
+                throw new Exception("No More Questions available please");
+
+            }
 
         }
+        public Question GetNextQuestion()
+        {
+            if (_currentQuestionIndex == _questionList.Count())
+            {
+                throw new Exception("No More Questions Available");
+            }
+
+
+            _currentQuestionIndex++;
+            return GetQuestionWithoutAnswer();
+
+        }
+
+
+        public String ForAnswer()
+        {
+            if (_currentQuestionIndex <= 5)
+            {
+                MultipleChoiceQuestion question = (MultipleChoiceQuestion)_questionList[_currentQuestionIndex];
+                String options = question.CorrectAnswer;
+                return options;
+            }
+            else
+            {
+                Question question = _questionList[_currentQuestionIndex];
+                String options = question.CorrectAnswer;
+                return options;
+            }
+        }
+
+
+        public bool CheckUserAnswer( string userAnswer)
+        {
+            if (_currentQuestionIndex <= 5)
+            {
+                bool Final = userAnswer == ForAnswer();
+                if (Final == true)
+                {
+                    Score++;
+                    
+                }
+
+                return Final;
+
+            }
+            else
+            {
+                if (userAnswer == ForAnswer())
+                {
+                    bool result = true;
+                    Score++;
+                    
+                    return result;
+                }
+
+                else
+                {
+                    bool result = false;
+                    return result;
+                }
+
+            }
+
+
+
+
+        }
+        public int ForTotalPoints()
+        {
+            int Points = 0;
+            for (int i = 0; i < _questionList.Count(); i++)
+            {
+                Question question = (Question)_questionList[i];
+                Points += question.Points;
+            }
+            return Points;
+        }
+
+       
+
+
+        /* private void LoadQuestions()
+         {
+             // Add 5 multiple choice questions
+             _questionList1.Add(new MultipleChoiceQuestion("Which country held 2022 football worldcup?", "Qatar", 1, new string[] { "USA", "China", "Canada", "Qatar" }));
+             _questionList1.Add(new MultipleChoiceQuestion("What is the nation capital of India ?", "Delhi", 1, new string[] { "Maharashtra", "Rajasthan", "Kerala" }));
+             _questionList1.Add(new MultipleChoiceQuestion("Which is the largest mammal?", "Blue Whale", 1, new string[] { "Elephant", "Giraffe", "Hippopotamus" }));
+             _questionList1.Add(new MultipleChoiceQuestion("What team won 2022 football worldcup ?", "Argentina", 1, new string[] { "Germany", "France", "Morocco" }));
+             _questionList1.Add(new MultipleChoiceQuestion("Which country is known as land of maple syrup", "Canada", 1, new string[] { "Chine", "USA", "Russia","Canada" }));
+
+             // Add 5 true/false questions
+             _questionList2.Add(new TrueFalseQuestion("The Great Wall of China is visible from space.", "false", 1, new string[] {"true", "false"}));
+             _questionList2.Add(new TrueFalseQuestion("The human body has three lungs.", "false", 1, new string[] { "true", "false" }));
+             _questionList2.Add(new TrueFalseQuestion("The Earth is square.", "false", 1, new string[] { "true", "false" }));
+             _questionList2.Add(new TrueFalseQuestion("The Statue of Liberty is in Germany.", "false", 1, new string[] { "true", "false" }));
+             _questionList2.Add(new TrueFalseQuestion("The sun rises in the east and sets in the west.", "true", 1, new string[] { "true", "false" }));
+
+         }*/
 
         /*
         public Question GetNextQuestion()
@@ -111,48 +258,50 @@ namespace Interactive_Quiz_By_Aditya_Sharma
         }
         */
 
-        public MultipleChoiceQuestion GetNextQuestion()
-        {
-            MultipleChoiceQuestion currentQues = new MultipleChoiceQuestion();
-            _currentQuestion++;
-            if (_currentQuestion < _questionList1.Count())
-            {
-                currentQues = GetMCQQuestionWithoutAnswer(_currentQuestion);
-            }
-            return currentQues;
-        }
+        /* public MultipleChoiceQuestion GetNextQuestion()
+         {
+             MultipleChoiceQuestion currentQues = new MultipleChoiceQuestion();
+             _currentQuestion++;
+             if (_currentQuestion < _questionList1.Count())
+             {
+                 currentQues = GetMCQQuestionWithoutAnswer(_currentQuestion);
+             }
+             return currentQues;
+         }
 
-        private MultipleChoiceQuestion GetMCQQuestionWithoutAnswer(int currentQuestion)
-        {
+         private MultipleChoiceQuestion GetMCQQuestionWithoutAnswer(int currentQuestion)
+         {
 
-            quesWithOptions.QuestionText = _questionList1[currentQuestion].QuestionText;
-            quesWithOptions.Points = _questionList1[currentQuestion].Points;
-            quesWithOptions.CorrectAnswer = "";
-            quesWithOptions.options = _questionList1[currentQuestion].options;
-            return quesWithOptions;
-        }
+             quesWithOptions.QuestionText = _questionList1[currentQuestion].QuestionText;
+             quesWithOptions.Points = _questionList1[currentQuestion].Points;
+             quesWithOptions.CorrectAnswer = "";
+             quesWithOptions.options = _questionList1[currentQuestion].options;
+             return quesWithOptions;
+         }
 
-        private TrueFalseQuestion GetTFQuestionWithoutAnswer(int currentQuestion)
-        {
-            tfQuesWithOptions.QuestionText = _questionList2[currentQuestion].QuestionText;
-            tfQuesWithOptions.Points = _questionList2[currentQuestion].Points;
-            tfQuesWithOptions.CorrectAnswer = "";
-            return tfQuesWithOptions;
-        }
+         private TrueFalseQuestion GetTFQuestionWithoutAnswer(int currentQuestion)
+         {
+             tfQuesWithOptions.QuestionText = _questionList2[currentQuestion].QuestionText;
+             tfQuesWithOptions.Points = _questionList2[currentQuestion].Points;
+             tfQuesWithOptions.CorrectAnswer = "";
+             return tfQuesWithOptions;
+         }*/
 
-        public void CheckUserAnswer(Question question, string userAnswer)
-        {
-            if(question.CorrectAnswer == userAnswer)
-            {
-                Score++;
-            }
-        }
+        //public void CheckUserAnswer(Question question, string userAnswer)
+        //{
+        //    if(question.CorrectAnswer == userAnswer)
+        //    {
+        //        Score++;
+        //    }
+        //}
     }
 
 
         //public virtual bool CheckAnswer(string userAnswer)
         //{
         //    //return userAnswer.ToLower() == Answer
+
+
 }
 
       
